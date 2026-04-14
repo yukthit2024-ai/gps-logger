5b7fafa8b5311510d38c39eb4784f219ea28fcdc
Shibu TN <tnshibu@yahoo.com>
2026-04-14 23:39:18 +0530

    version tag in About page

--- app/src/main/java/com/example/gpslocationlogger/AboutActivity.java
+++ app/src/main/java/com/example/gpslocationlogger/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.example.gpslocationlogger;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
@@ -8,6 +9,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView tvAboutContent = findViewById(R.id.tvAboutContent);
+        String buildInfo = "GPS Location Logger\n" +
+                "Timestamp: " + BuildConfig.BUILD_TIMESTAMP + "\n" +
+                "Commit: " + BuildConfig.GIT_SHA + "\n" +
+                "Tag: " + BuildConfig.GIT_TAG + "\n\n" +
+                getString(R.string.about_description);
+        tvAboutContent.setText(buildInfo);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle(R.string.menu_about);
